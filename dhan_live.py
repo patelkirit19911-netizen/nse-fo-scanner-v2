fa8976582d539d3a7ed04a18ce99fdfdd9f0a823
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-06-29 20:58:19 +0530

    Add test_connection function for Dhan API

--- dhan_live.py
+++ dhan_live.py
@@ -5,3 +5,7 @@ CLIENT_ID = os.getenv("DHAN_CLIENT_ID")
 ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
 
 dhan = dhanhq(CLIENT_ID, ACCESS_TOKEN)
+
+
+def test_connection():
+    return "Dhan API Connected Successfully"
