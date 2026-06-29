f8d179e28ef909acc654f6b3d08759a296fc92bf
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-06-29 19:27:32 +0530

    Add DHAN API client initialization

--- market_data.py
+++ market_data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import requests
 from config import CSV_URL
+from dhanhq import dhanhq
+from config import DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN
 
 def load_scrip_master():
     """Load Dhan Scrip Master"""
     return pd.read_csv(CSV_URL, low_memory=False)
-
+dhan = dhanhq(DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN)
 def get_fno_stocks():
     df = load_scrip_master()
 
