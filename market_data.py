37893b2e9b00f354a48157ad86935698194cb9b7
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-06-29 17:51:27 +0530

    Add functions to load scrip master and filter F&O stocks

--- market_data.py
+++ market_data.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import requests
+from config import CSV_URL
 
+def load_scrip_master():
+    """Load Dhan Scrip Master"""
+    return pd.read_csv(CSV_URL, low_memory=False)
+
+def get_fno_stocks():
+    df = load_scrip_master()
+
+    # केवल NSE F&O स्टॉक्स
+    df = df[
+        (df["SEM_EXM_EXCH_ID"] == "NSE") &
+        (df["SEM_INSTRUMENT_NAME"].isin(["FUTSTK", "OPTSTK", "FUTIDX", "OPTIDX"]))
+    ]
+
+    return df
