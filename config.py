1a9ae6861717e6a426be35898c904a66249740c7
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-06-29 17:35:34 +0530

    Add configuration variables for Dhan client and bot

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+DHAN_CLIENT_ID = os.getenv("DHAN_CLIENT_ID")
+DHAN_ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+CSV_URL = "https://images.dhan.co/api-data/api-scrip-master.csv"
