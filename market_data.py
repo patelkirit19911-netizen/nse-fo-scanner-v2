d8de8ea7ad23d2b8728061bb2f7ea4e63588e99b
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-06-30 10:23:53 +0530

    Refactor market_data.py to use dhan.quote_data

--- market_data.py
+++ market_data.py
@@ -1,19 +1,14 @@
 import pandas as pd
-import requests
+from dhanhq import dhanhq
+from config import CSV_URL, DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN
 
-from config import CSV_URL
-from dhanhq import dhanhq, DhanContext
-from config import DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN
-
-dhan_context = DhanContext(DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN)
-dhan = dhanhq(dhan_context)
+dhan = dhanhq(DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN)
 
 
 def load_scrip_master():
-    """Load Dhan Scrip Master"""
     return pd.read_csv(CSV_URL, low_memory=False)
-def test_dhan():
-    return "✅ Dhan API Connected"
+
+
 def get_fno_stocks():
     df = load_scrip_master()
 
@@ -23,26 +18,12 @@ def get_fno_stocks():
     ]
 
     df = df.drop_duplicates(subset=["SEM_TRADING_SYMBOL"])
-
     return df.reset_index(drop=True)
+
+
 def get_live_quotes(security_ids):
-    url = "https://api.dhan.co/v2/marketfeed/ohlc"
-
-    headers = {
-    "access-token": DHAN_ACCESS_TOKEN.strip(),
-    "client-id": str(DHAN_CLIENT_ID).strip(),
-    "Content-Type": "application/json"
-    }
-
-    print(repr(headers))
-
     payload = {
         "NSE_FNO": security_ids
     }
-    print("CLIENT_ID =", repr(DHAN_CLIENT_ID))
-    print("TOKEN =", repr(DHAN_ACCESS_TOKEN))
-    print("CLIENT_ID TYPE =", type(DHAN_CLIENT_ID))
-    print("TOKEN TYPE =", type(DHAN_ACCESS_TOKEN))
-    
-    response = requests.post(url, headers=headers, json=payload)
-    return response.json()
+
+    return dhan.quote_data(payload)
