9e716cbadb5780039cfbb3c4228dfb28e85605f4
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-01 22:45:56 +0530

    Update scanner_v2.py

--- scanner_v2.py
+++ scanner_v2.py
@@ -85,14 +85,14 @@ merged_df["buy_sell_ratio"] = (
  #V3 Pro Confidence Score
 
  #Price above VWAP
- merged_df.loc[merged_df["last_price"] > merged_df["vwap"], "score"] += 20
+ #merged_df.loc[merged_df["last_price"] > merged_df["vwap"], "score"] += 20
 
  #EMA Trend
  #EMA Trend Score
-merged_df.loc[
-    merged_df["last_price"] > merged_df["ema20"],
-    "score"
-] += 20
+#merged_df.loc[
+   # merged_df["last_price"] > merged_df["ema20"],
+   # "score"
+#] += 20
 
 
  #Volume Strength
