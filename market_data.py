54dd8fa9424548320e15eacf53b0cadfadee93d4
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-01 21:25:45 +0530

    Rename function get_fno_stocks to get_nifty_stocks

--- market_data.py
+++ market_data.py
@@ -13,7 +13,7 @@ def load_scrip_master():
     return pd.read_csv(CSV_URL, low_memory=False)
 
 
-def get_fno_stocks():
+def get_nifty_stocks():
     df = load_scrip_master()
     df = df[
     (df["SEM_EXM_EXCH_ID"] == "NSE") &
