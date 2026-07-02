f2941da7d9ca73a6e163f1482902a256946e445a
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-02 09:16:09 +0530

    Uncomment price and EMA trend score calculations

--- scanner_v2.py
+++ scanner_v2.py
@@ -84,15 +84,15 @@ merged_df["buy_sell_ratio"] = (
 merged_df["score"] = 0
  #V3 Pro Confidence Score
 
- #Price above VWAP
- #merged_df.loc[merged_df["last_price"] > merged_df["vwap"], "score"] += 20
+Price above VWAP
+merged_df.loc[merged_df["last_price"] > merged_df["vwap"], "score"] += 20
 
  #EMA Trend
  #EMA Trend Score
-#merged_df.loc[
-   # merged_df["last_price"] > merged_df["ema20"],
-   # "score"
-#] += 20
+merged_df.loc[
+    merged_df["last_price"] > merged_df["ema20"],
+    "score"
+] += 20
 
 
  #Volume Strength
