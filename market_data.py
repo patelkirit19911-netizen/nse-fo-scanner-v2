da558e178d84c350b4a2a4a86082e4c50dd69916
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-03 12:04:19 +0530

    Refactor get_nifty_stocks to simplify filtering
    Removed unnecessary filter for SEM_INSTRUMENT_NAME in get_nifty_stocks function.

--- market_data.py
+++ market_data.py
@@ -17,10 +17,8 @@ def get_nifty_stocks():
     df = load_scrip_master()
     df = df[
     (df["SEM_EXM_EXCH_ID"] == "NSE") &
-    (df["SEM_INSTRUMENT_NAME"] == "EQUITY") &
-    (~df["SEM_TRADING_SYMBOL"].str.contains("NSETEST", na=False))
-]
-
+(df["SEM_SEGMENT"] == "D") &
+(~df["SEM_TRADING_SYMBOL"].str.contains("NSETEST", na=False))
     df = df.drop_duplicates(subset=["SEM_TRADING_SYMBOL"])
     nifty50 = [
     "RELIANCE","TCS","INFY","HDFCBANK","ICICIBANK","SBIN",
