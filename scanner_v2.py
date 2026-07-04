d91c58e0a823717e39ac6c34c6eb090978bd3eb1
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-04 16:31:16 +0530

    Fix indentation issue in breakout candle assignment

--- scanner_v2.py
+++ scanner_v2.py
@@ -203,7 +203,7 @@ today_df = history_df[
     )
 ]
 
- breakout_candle = today_df[today_df["close"] > previous_week_high]
+breakout_candle = today_df[today_df["close"] > previous_week_high]
 
 buy_signal = len(breakout_candle) > 0
 signal_key = (
