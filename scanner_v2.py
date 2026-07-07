062d3835bd8da2a2fa2c801a25ab212f75bc4a6b
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-07 13:52:22 +0530

    Update buy signal calculation logic
    Refactor buy signal logic to include yesterday's close comparison.

--- scanner_v2.py
+++ scanner_v2.py
@@ -166,13 +166,16 @@ for _, row in scanner.iterrows():
     print("Previous Week High:", previous_week_high)
     print("Today's High:", today_df["high"].max())
     print("Today's Close:", today_df["close"].max())
+    yesterday_df = history_df[history_df.index.date < last_date.date()]
+    yesterday_close = yesterday_df["close"].iloc[-1]
     print("Previous Week High:", previous_week_high)
     breakout_candle = today_df[
-    (today_df["close"].shift(1) <= previous_week_high) &
     (today_df["close"] > previous_week_high)
     ]
-
-    buy_signal = not breakout_candle.empty
+    buy_signal = (
+    yesterday_close <= previous_week_high
+    and
+    not breakout_candle.empty)
 
     print(row["SEM_TRADING_SYMBOL"], previous_week_high, len(breakout_candle))
     print("Buy Signal:", buy_signal)
